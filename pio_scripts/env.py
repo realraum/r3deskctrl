bdeb2abba62b8484bae0cdaf83d5ca6ff538112b
CommanderRedYT <ccommanderred@gmail.com>
2025-10-15 20:37:12 +0200

    Remove logs

--- pio_scripts/env.py
+++ pio_scripts/env.py
@@ -9,9 +9,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-print("WiFI SSID: ",  os.getenv("WIFI_SSID"))
-print("WiFI Password: ",  os.getenv("WIFI_PASSWORD"))
-
 env.Append(CPPDEFINES=[
     ("OTA_SERVER_BASE_URL", env.StringifyMacro(os.getenv("OTA_SERVER_BASE_URL"))),
     ("OTA_PASSWORD", env.StringifyMacro(os.getenv("OTA_PASSWORD"))),
